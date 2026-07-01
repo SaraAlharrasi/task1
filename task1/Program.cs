892ff24094c3656275197500bac031448c02641f
SaraAlharrasi <saraalharrasi00@gmail.com>
2026-07-01 09:35:01 +0400

    first exercise

--- task1/Program.cs
+++ task1/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // Task 1 exercise 1 
+        //declaring variables 
+        string name = "Maryam";
+        int age = 40;
+        double height = 160;
+        bool IsStudent = false;
+        
+        //print out personal card info 
+        
+        Console.WriteLine("///////// card info /////////");
+        
+        Console.WriteLine("Name: " +  name);
+        Console.WriteLine("Age: " +  age);
+        Console.WriteLine("height: " +  height);
+        Console.WriteLine("student: " +  IsStudent);
+        
     }
 }
