cc38abc94b683fd83a66553a9ee1f5c4a73317d6
SaraAlharrasi <saraalharrasi00@gmail.com>
2026-07-01 10:07:29 +0400

    added writeline to make it clear

--- task 1 ex2/Program.cs
+++ task 1 ex2/Program.cs
@@ -7,7 +7,10 @@ class Program
         // task 1 exercise 2 
         
         // taking input from the user 
+        
+        Console.WriteLine("please enter the first number");
         int recLength = int.Parse(Console.ReadLine());
+        Console.WriteLine("please enter the second number");
         int recWidth = int.Parse(Console.ReadLine());
         
         // calculating area and perimeter 
