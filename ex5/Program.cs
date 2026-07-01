5e55f80380fe49a8cf7dd982d2c71c33b04467e5
SaraAlharrasi <saraalharrasi00@gmail.com>
2026-07-01 11:02:18 +0400

    exercise 5

--- ex5/Program.cs
+++ ex5/Program.cs
@@ -4,6 +4,34 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // ask the user for the grade 
+        
+        Console.WriteLine("Please Enter your grade (A,B,C,D,F)");
+        char grade = char.Parse(Console.ReadLine());
+
+        switch (grade)
+        {
+            case 'A':
+                Console.WriteLine("excellent");
+                break;
+
+            case 'B':
+                Console.WriteLine("Very good");
+                break;
+            
+            case 'C':
+                Console.WriteLine("Good");
+                break;
+            case 'D':
+                Console.WriteLine("Pass");
+                break;
+            case 'F':
+                Console.WriteLine("Fail");
+                break;
+            default:
+                Console.WriteLine("Invalid grade");
+                break;
+        }
+
     }
 }
