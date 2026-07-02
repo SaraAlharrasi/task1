ed6d91dd31a203e79280414c974fea8d80c25c24
SaraAlharrasi <saraalharrasi00@gmail.com>
2026-07-02 09:31:02 +0400

    task 2 done

---
+++ task 2/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace task_2;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        /*
+        /////////////////////// Task 1 ////////////////////////////////////////
+        
+        Console.WriteLine("please enter a starting number: ");
+        int num = int.Parse(Console.ReadLine());
+
+        for (; num >= 1; num--)
+        {
+            Console.WriteLine(num);
+        }
+        Console.WriteLine("liftoff ");
+        */
+        
+        
+        
+        
+        /////////////////////// Task 2 ////////////////////////////////////////
+        
+        
+        Console.WriteLine("please enter a positive whole number: ");
+        int N = int.Parse(Console.ReadLine());
+        int sum = 0;
+
+        for (int i = 1; i <= N ; i++)
+        {
+            sum += i;
+        }
+        Console.WriteLine("final sum is : " + sum);
+    }
+}
