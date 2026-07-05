47d0834e08c0ddf84bea758447913fdabc100696
SaraAlharrasi <saraalharrasi00@gmail.com>
2026-07-05 15:27:43 +0400

    implemented task 2

--- task 1 part 3/Program.cs
+++ task 1 part 3/Program.cs
@@ -5,7 +5,7 @@ class Program
     static void Main(string[] args)
     {
         ///////////////////////task 1 ///////////////////////////
-        
+        /*
         Console.WriteLine("Please enter the first number: ");
         int firstNumber = int.Parse(Console.ReadLine());
         Console.WriteLine("Please enter the second number: ");  
@@ -13,5 +13,21 @@ class Program
         
         int subtraction = Math.Abs(secondNumber - firstNumber);
         Console.WriteLine("The subtraction result is: " + subtraction);
+        */
+        
+        
+        ///////////////////////task 2 ///////////////////////////
+        
+        Console.WriteLine("Please enter a number: ");
+        int number = int.Parse(Console.ReadLine());
+        
+        double sqr = Math.Sqrt(number);
+        Console.WriteLine("The square root of " + number + " = " + sqr);
+
+        double pow = Math.Pow(number, 2);
+        Console.WriteLine("The square  of " + number + " = " + pow);
+        
+        
+
     }
 }
