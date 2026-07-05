dd27f09bc0d179d5f2ca1c3cf0213417c2eed900
SaraAlharrasi <saraalharrasi00@gmail.com>
2026-07-05 15:20:42 +0400

    implemented task 1

---
+++ task 1 part 3/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace task_1_part_3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        ///////////////////////task 1 ///////////////////////////
+        
+        Console.WriteLine("Please enter the first number: ");
+        int firstNumber = int.Parse(Console.ReadLine());
+        Console.WriteLine("Please enter the second number: ");  
+        int secondNumber = int.Parse(Console.ReadLine());
+        
+        int subtraction = Math.Abs(secondNumber - firstNumber);
+        Console.WriteLine("The subtraction result is: " + subtraction);
+    }
+}
