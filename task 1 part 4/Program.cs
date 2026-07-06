36b0cc4ebb621795668853dc9841ce5e424159c2
SaraAlharrasi <saraalharrasi00@gmail.com>
2026-07-06 18:06:50 +0400

    task 5 done

--- task 1 part 4/Program.cs
+++ task 1 part 4/Program.cs
@@ -14,12 +14,28 @@ class Program
         sqr = sqr * sqr;
         return sqr;
     }
-    */
+   
     //--------------------------------------- task 3 -----------------------------
     static double CelsiusToFahrenheit(double celsius)
     {
         double Fahrenheit = (celsius * 9 / 5) + 32;
         return Fahrenheit;
+    }
+     
+    //--------------------------------------- task 4 -----------------------------
+    static void DisplayMenu()
+    {
+        Console.WriteLine("1: star");
+        Console.WriteLine("2: Help");
+        Console.WriteLine("3: Exit");
+        
+    }
+    */
+    //--------------------------------------- task 5 -----------------------------
+    static bool IsEven(int number)
+    {
+        return number % 2 ==0 ;
+        
     }
     static void Main(string[] args)
     {
@@ -34,7 +50,7 @@ class Program
         
         
         Console.WriteLine("The answer is : " +  square(num));
-        */
+  
         
         
         //--------------------------------------- task 3 -----------------------------
@@ -42,5 +58,24 @@ class Program
         double celsius = double.Parse(Console.ReadLine());
         
         Console.WriteLine("The Fahrenhite is:  " + CelsiusToFahrenheit(celsius));
+              
+        
+        //--------------------------------------- task 4 -----------------------------
+        
+        DisplayMenu();
+        */
+        
+        //--------------------------------------- task 5 -----------------------------
+        Console.WriteLine("enter a number");
+        int number = int.Parse(Console.ReadLine());
+
+        if (IsEven(number))
+        {
+            Console.WriteLine("even");
+        }
+        else
+        {
+            Console.WriteLine("odd");
+        }
     }
 }
