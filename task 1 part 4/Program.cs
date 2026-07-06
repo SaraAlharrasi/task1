a133e04cb2bee0e9a186f6be9cd34c4cf6ede15c
SaraAlharrasi <saraalharrasi00@gmail.com>
2026-07-06 17:29:23 +0400

    task 1 done

---
+++ task 1 part 4/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace task_1_part_4;
+
+class Program
+{
+    
+    static void PrintWelcome(string userName)
+    {
+        Console.WriteLine("Welcome " + userName);
+    }
+    
+    static void Main(string[] args)
+    {
+        Console.WriteLine("enter your name : ");
+        string name = Console.ReadLine();
+        PrintWelcome(name);
+    }
+}
