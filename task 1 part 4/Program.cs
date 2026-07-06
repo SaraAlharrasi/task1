be819df343f433efe090636d2d4fde2f2fbbc442
SaraAlharrasi <saraalharrasi00@gmail.com>
2026-07-06 17:47:03 +0400

    task 3 done

--- task 1 part 4/Program.cs
+++ task 1 part 4/Program.cs
@@ -7,12 +7,19 @@ class Program
     {
         Console.WriteLine("Welcome " + userName);
     }
-    */
+    
     //--------------------------------------- task 2 -----------------------------
     static int square(int sqr)
     {
         sqr = sqr * sqr;
         return sqr;
+    }
+    */
+    //--------------------------------------- task 3 -----------------------------
+    static double CelsiusToFahrenheit(double celsius)
+    {
+        double Fahrenheit = (celsius * 9 / 5) + 32;
+        return Fahrenheit;
     }
     static void Main(string[] args)
     {
@@ -20,14 +27,20 @@ class Program
         /*Console.WriteLine("enter your name : ");
         string name = Console.ReadLine();
         PrintWelcome(name);
-        */
+        
         //--------------------------------------- task 2 -----------------------------
         Console.WriteLine("enter a number ");
         int num = int.Parse(Console.ReadLine());
         
         
         Console.WriteLine("The answer is : " +  square(num));
+        */
         
         
+        //--------------------------------------- task 3 -----------------------------
+        Console.WriteLine("enter celsius: ");
+        double celsius = double.Parse(Console.ReadLine());
+        
+        Console.WriteLine("The Fahrenhite is:  " + CelsiusToFahrenheit(celsius));
     }
 }
