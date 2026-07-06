40b4c5bc234c4d0ddd138070ed6d935aeff8c08a
SaraAlharrasi <saraalharrasi00@gmail.com>
2026-07-06 17:40:17 +0400

    task 2 done

--- task 1 part 4/Program.cs
+++ task 1 part 4/Program.cs
@@ -2,16 +2,32 @@
 
 class Program
 {
-    
-    static void PrintWelcome(string userName)
+    //--------------------------------------- task 1 -----------------------------
+    /*static void PrintWelcome(string userName)
     {
         Console.WriteLine("Welcome " + userName);
     }
-    
+    */
+    //--------------------------------------- task 2 -----------------------------
+    static int square(int sqr)
+    {
+        sqr = sqr * sqr;
+        return sqr;
+    }
     static void Main(string[] args)
     {
-        Console.WriteLine("enter your name : ");
+        //--------------------------------------- task 1 -----------------------------
+        /*Console.WriteLine("enter your name : ");
         string name = Console.ReadLine();
         PrintWelcome(name);
+        */
+        //--------------------------------------- task 2 -----------------------------
+        Console.WriteLine("enter a number ");
+        int num = int.Parse(Console.ReadLine());
+        
+        
+        Console.WriteLine("The answer is : " +  square(num));
+        
+        
     }
 }
