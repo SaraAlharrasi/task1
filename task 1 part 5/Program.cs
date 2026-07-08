94ed78a6d0f01fa5de93274e57063f27841a6f80
SaraAlharrasi <saraalharrasi00@gmail.com>
2026-07-08 09:37:15 +0400

    task 1 done

---
+++ task 1 part 5/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace task_1_part_5;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        //-------------------------------------- Task 1 -----------------------------------------------
+        int[] studentGrades = new int[5];
+
+        int counter = 0;
+
+
+        for (counter = 0; counter < studentGrades.Length; counter++)
+        {
+            try
+            {
+                Console.WriteLine($"Enter student {counter + 1} grade");
+                studentGrades[counter] = int.Parse(Console.ReadLine());
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid input");
+            }
+            
+        }
+
+        foreach (int grade in studentGrades)
+        {
+            Console.WriteLine($"grade: {grade}");
+        }
+        
+        //-------------------------------------- Task 2 -----------------------------------------------
+        
+        
+        
+        
+        
+    }
+}
